d321bb8a3fa230fa1265f55469506775a4eb2cd9
ASNaeem <sleepless.naeem@gmail.com>
2019-04-08 14:49:43 +0600

    new routes

--- server/app.js
+++ server/app.js
@@ -9,4 +9,18 @@ app.get('/', (req, res) => {
     res.render('index', {pretty: true});
 })
 
+app.post('/', (req, res) => {
+    console.log(req.body.username)
+    res.redirect(`/user/${req.body.username}`)
+})
+app.get('/user/:username', (req, res) => {
+    res.send(req.params.username)
+})
+
+app.get('/about',(req, res) => {
+    res.render("about",{pretty: false});
+})
+app.get('/*', (req, res)=>{
+    res.send("request not found: 404")
+})
 app.listen(3000, 'localhost', () => console.log('Server started'))
